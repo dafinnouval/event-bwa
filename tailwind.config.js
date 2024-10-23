91bbf39d86734baa74373acc1054f4ec7aafac12
ouval <dafinnovalarif1@gmail.com>
2024-10-23 18:00:44 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "russian-violet": "#1F1449",
+        iris: "#5C40CC",
+        manatee: "#9698A9",
+      },
+    },
+  },
+  plugins: [],
+};
